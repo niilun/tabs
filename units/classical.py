d7a03cc6c58131e52c95678c2b4ab6bf53101646
Leowondeh <49104843+Leowondeh@users.noreply.github.com>
2024-11-10 21:33:19 +0200

    Fix bad naming in comment

--- units/classical.py
+++ units/classical.py
@@ -2,7 +2,7 @@ from .base import *
 import logging
 
 # +---------------------+
-# |  ancient era units  |
+# |  classic era units  |
 # |  TODO: add all      |
 # |  units info         |
 # +---------------------+
