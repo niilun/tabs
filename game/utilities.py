48f9f56206ee8f85c5befa54d998b6ec56e6414b
niilun <49104843+niilun@users.noreply.github.com>
2025-06-06 19:02:37 +0300

    fix: ui paths in some elements

--- game/utilities.py
+++ game/utilities.py
@@ -74,15 +74,15 @@ def reload_game():
 
 def show_error(title: str, message: str):
     from CTkMessagebox import CTkMessagebox
-    CTkMessagebox(title = title, message = message, icon = 'assets/ui/exit_16x.png')
+    CTkMessagebox(title = title, message = message, icon = 'assets/ui/exit.png')
 
 def show_info(title: str, message: str):
     from CTkMessagebox import CTkMessagebox
-    CTkMessagebox(title = title, message = message, icon = 'assets/ui/info_16x.png')
+    CTkMessagebox(title = title, message = message, icon = 'assets/ui/info.png')
 
 def show_ask_question(title: str, question: str) -> bool:
     from CTkMessagebox import CTkMessagebox
-    box = CTkMessagebox(title = title, message = question, icon = 'assets/ui/settings_64x.png', option_1 = 'Yes', option_2 = 'No')
+    box = CTkMessagebox(title = title, message = question, icon = 'assets/ui/settings.png', option_1 = 'Yes', option_2 = 'No')
     response = box.get()
 
     if response == 'Yes':
